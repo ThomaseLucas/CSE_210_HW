09a8b137e16d8fb961ce08b7b9199551413b5f4a
thomaselucas <thomaselucas2020@gmail.com>
2023-04-21 19:42:23 -0600

    made a program to print name like James Bond

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,9 @@
-using System;
+//Make code to introduce the user as just like James Bond.//
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Sandbox World!");
-    }
-}
+Console.WriteLine("What is your first name? ");
+string f_name = Console.ReadLine();
+
+Console.WriteLine("What is your last name? ");
+string l_name = Console.ReadLine();
+
+Console.WriteLine($"Your name is {l_name}, {f_name} {l_name}.");
