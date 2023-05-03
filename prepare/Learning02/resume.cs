512073b02024f5ebc0705cabd61e602049507bf5
thomaselucas <thomaselucas2020@gmail.com>
2023-05-03 15:56:13 -0600

    Display a list of jobs in a resume

---
+++ prepare/Learning02/resume.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+{
+    
+};
+public class Resume
+{
+    //string _nameOfUser;
+    public List<Job> _jobs;
+
+    public Resume()
+    {
+        _jobs = new List<Job>();
+    }
+    
+}
